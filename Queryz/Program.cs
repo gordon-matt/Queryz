a044d2f0b54619724e719c568d199579d9e43cac
Matt Gordon <gordon_matt@hotmail.com>
2025-05-31 09:31:38 +0930

    Solved an issue with the identity UI no longer working

--- Queryz/Program.cs
+++ Queryz/Program.cs
@@ -1,13 +1,123 @@
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Extenso.AspNetCore.Mvc.ExtensoUI;
+using Extenso.AspNetCore.Mvc.ExtensoUI.Providers;
+using Extenso.AspNetCore.Mvc.Rendering;
+using Extenso.AspNetCore.OData;
+using Extenso.Data.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Queryz.Data;
+using Queryz.Data.Entities;
+using Queryz.Data.TransformFunctions;
+using Queryz.Infrastructure;
+using Queryz.Services;
 
-namespace Queryz;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add Autofac
+builder.Host
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterType<ApplicationDbContextFactory>().As<IDbContextFactory>().SingleInstance();
+
+        containerBuilder.RegisterGeneric(typeof(EntityFrameworkRepository<>))
+            .As(typeof(IRepository<>))
+            .InstancePerLifetimeScope();
+
+        containerBuilder.RegisterType<QueryzRazorViewRenderService>().As<IRazorViewRenderService>().SingleInstance();
+        containerBuilder.RegisterType<ODataRegistrar>().As<IODataRegistrar>().SingleInstance();
+
+        containerBuilder.RegisterType<DataSourceService>().As<IDataSourceService>().InstancePerDependency();
+        containerBuilder.RegisterType<EnumerationService>().As<IEnumerationService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportService>().As<IReportService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportTableService>().As<IReportTableService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportTableColumnService>().As<IReportTableColumnService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportGroupService>().As<IReportGroupService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportGroupRoleService>().As<IReportGroupRoleService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportSortingService>().As<IReportSortingService>().InstancePerDependency();
+        containerBuilder.RegisterType<ReportUserBlacklistService>().As<IReportUserBlacklistService>().InstancePerDependency();
+
+        // Transform Functions
+        containerBuilder.RegisterType<BeautifyJsonFunction>().As<ITransformFunction>().SingleInstance();
+        containerBuilder.RegisterType<ConvertTimeZoneFunction>().As<ITransformFunction>().SingleInstance();
+        //containerBuilder.RegisterType<RoundUpFunction>().As<ITransformFunction>().SingleInstance();
+
+        containerBuilder.RegisterType<ReportBuilderService>().As<IReportBuilderService>().InstancePerDependency();
+    });
+
+// Configure services
+builder.Services.AddAutofac();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Identity setup
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
 {
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders()
+.AddDefaultUI();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+builder.Services
+    .AddControllersWithViews()
+    .AddApplicationPart(typeof(Microsoft.AspNetCore.Identity.UI.LoggerEventIds).Assembly)
+    .AddNewtonsoftJson()
+    .AddOData((options, serviceProvider) =>
+    {
+        options.Select().Expand().Filter().OrderBy().SetMaxTop(null).Count();
+        var registrars = serviceProvider.GetRequiredService<IEnumerable<IODataRegistrar>>();
+        foreach (var registrar in registrars)
+        {
+            registrar.Register(options);
+        }
+    });
+
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseExtensoUI<Bootstrap4UIProvider>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// IMPORTANT: This session call MUST go before UseMvc()
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
